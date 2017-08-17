5f20f39a1353d1760fc0a4c9ff79c9f18a19cead
Frederic Rolland <frolland@euronext.com>
2017-08-17 18:19:37 +0200

    Process also gzipped files

--- parsing/logparser.go
+++ parsing/logparser.go
@@ -2,6 +2,7 @@ package parsing
 
 import (
 	"bufio"
+	"compress/gzip"
 	"log"
 	"os"
 )
@@ -18,7 +19,28 @@ func ParseLines(filePath string, parse func(string) (string, bool)) error {
 	}
 	defer inputFile.Close()
 
-	scanner := bufio.NewScanner(inputFile)
+	bReader := bufio.NewReader(inputFile)
+	testBytes, err := bReader.Peek(2) //read 2 bytes
+	if nil != err {
+		log.Fatal("FATAL: ", err)
+		return err
+	}
+
+	var scanner *bufio.Scanner
+	if testBytes[0] == 31 && testBytes[1] == 139 {
+		// gzipped file
+		gzipReader, err := gzip.NewReader(bReader)
+		if err != nil {
+			log.Fatal("FATAL: ", err)
+			return err
+		}
+		scanner = bufio.NewScanner(gzipReader)
+	} else {
+		//Not gzipped, just make a scanner based on the reader
+		scanner = bufio.NewScanner(bReader)
+	}
+
+	//	scanner = bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		parse(scanner.Text())
 	}
